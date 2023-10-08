cfce0ccb72d762f7e0f4b976ce0d158ac717be58
Maksim Raspopov <raspopov_maksim@inbox.ru>
2023-10-08 12:27:10 +0500

    change main name

--- SwingMines/src/main/java/com/github/raspopov/SwingMines.java
+++ SwingMines/src/main/java/com/github/raspopov/SwingMines.java
@@ -4,10 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class MinesMain {
+public class SwingMines {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(MinesMain.class).headless(false).run(args);
+        new SpringApplicationBuilder(SwingMines.class).headless(false).run(args);
         // Throws headless exception
         // ApplicationContext context = SpringApplication.run(Mines.class);
     }
